f3008fed431574b0c6a07e2c8ecc8168585797e0
henripc <henriquechimetta@outlook.com>
2022-11-30 12:16:53 -0300

    Updated the register page.
    Added the errors messages, session and redirection.

--- register.php
+++ register.php
@@ -1,7 +1,11 @@
 <?php
 
 require_once('includes/config.php');
+require_once('includes/classes/Account.php');
+require_once('includes/classes/Constants.php');
 require_once('includes/classes/FormSanitizer.php');
+
+$account = new Account($connection);
 
 if (isset($_POST['submitButton'])) {
     $firstName = FormSanitizer::sanitizeFormString($_POST['firstName']);
@@ -11,6 +15,19 @@ if (isset($_POST['submitButton'])) {
     $email2 = FormSanitizer::sanitizeFormEmail($_POST['email2']);
     $password = FormSanitizer::sanitizeFormPassword($_POST['password']);
     $password2 = FormSanitizer::sanitizeFormPassword($_POST['password2']);
+
+    $success = $account->register($firstName, $lastName, $username, $email, $email2, $password, $password2);
+
+    if ($success) {
+        $_SESSION['userLoggedIn'] = $username;
+        header('Location: index.php');
+    }
+}
+
+function getInputValue(string $name): void
+{
+    if (isset($_POST[$name]))
+        echo $_POST[$name];
 }
 
 ?>
@@ -37,13 +54,32 @@ if (isset($_POST['submitButton'])) {
                 </div>
 
                 <form method="POST">
-                    <input type="text" name="firstName" placeholder="First name" required />
-                    <input type="text" name="lastName" placeholder="Last name" required />
-                    <input type="text" name="username" placeholder="Username" required />
-                    <input type="email" name="email" placeholder="Email" required />
-                    <input type="email" name="email2" placeholder="Confirm email" required />
+                    <?php echo $account->getError(Constants::$firstNameCharacters); ?>
+                    <input type="text" name="firstName" placeholder="First name"
+                        value="<?= getInputValue('firstName'); ?>" required />
+
+                    <?php echo $account->getError(Constants::$lastNameCharacters); ?>
+                    <input type="text" name="lastName" placeholder="Last name" value="<?= getInputValue('lastName'); ?>"
+                        required />
+
+                    <?php echo $account->getError(Constants::$usernameCharacters); ?>
+                    <?php echo $account->getError(Constants::$usernameTaken); ?>
+                    <input type="text" name="username" placeholder="Username" value="<?= getInputValue('username'); ?>"
+                        required />
+
+                    <?php echo $account->getError(Constants::$emailsDontMatch); ?>
+                    <?php echo $account->getError(Constants::$emailInvalid); ?>
+                    <?php echo $account->getError(Constants::$emailTaken); ?>
+                    <input type="email" name="email" placeholder="Email" value="<?= getInputValue('email'); ?>"
+                        required />
+                    <input type="email" name="email2" placeholder="Confirm email"
+                        value="<?= getInputValue('email2'); ?>" required />
+
+                    <?php echo $account->getError(Constants::$passwordsDontMatch); ?>
+                    <?php echo $account->getError(Constants::$passwordLength); ?>
                     <input type="password" name="password" placeholder="Password" required />
                     <input type="password" name="password2" placeholder="Confirm password" required />
+
                     <input type="submit" name="submitButton" value="SUBMIT" />
                 </form>
 
