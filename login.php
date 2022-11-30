cb56033ef3a1b5e7a34922099e3c65c42287cf58
henripc <henriquechimetta@outlook.com>
2022-11-30 12:17:48 -0300

    Updated the login page.
    Added errors messages, session and redirection.

--- login.php
+++ login.php
@@ -1,7 +1,28 @@
 <?php
 
+require_once('includes/config.php');
+require_once('includes/classes/Account.php');
+require_once('includes/classes/Constants.php');
+require_once('includes/classes/FormSanitizer.php');
+
+$account = new Account($connection);
+
 if (isset($_POST['submitButton'])) {
-    echo 'Form was submitted';
+    $username = FormSanitizer::sanitizeFormUsername($_POST['username']);
+    $password = FormSanitizer::sanitizeFormPassword($_POST['password']);
+
+    $success = $account->login($username, $password);
+
+    if ($success) {
+        $_SESSION['userLoggedIn'] = $username;
+        header('Location: index.php');
+    }
+}
+
+function getInputValue(string $name): void
+{
+    if (isset($_POST[$name]))
+        echo $_POST[$name];
 }
 
 ?>
@@ -28,7 +49,9 @@ if (isset($_POST['submitButton'])) {
                 </div>
 
                 <form method="POST">
-                    <input type="text" name="username" placeholder="Username" required />
+                    <?php echo $account->getError(Constants::$loginFailed); ?>
+                    <input type="text" name="username" placeholder="Username" value="<?= getInputValue('username'); ?>"
+                        required />
                     <input type="password" name="password" placeholder="Password" required />
                     <input type="submit" name="submitButton" value="SUBMIT" />
                 </form>
